ecc0f9292668b1b0982fa765d22812320f840d4a
Jack Lu <jacklu@jacklu.me>
2020-04-12 23:55:01 +0800

    +

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.addEventListener('DOMContentLoaded', function () {
+	ReactDOM.render(<h1>express-site-admin</h1>, document.body);
+});
