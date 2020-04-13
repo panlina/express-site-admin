6885d97bf399c2b9f17b922c34f492385113913f
Jack Lu <jacklu@jacklu.me>
2020-04-13 23:55:13 +0800

    +proxy rule

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 window.addEventListener('DOMContentLoaded', function () {
-	ReactDOM.render(<h1>express-site-admin</h1>, document.body);
+	ReactDOM.render(<Index />, document.body);
 });
+
+class Index extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			endpoint: "",
+			proxyRule: undefined,
+			proxyRuleLoading: false
+		};
+	}
+	connect() {
+		this.setState({ proxyRuleLoading: true });
+		axios.get(`${this.state.endpoint}/proxy-rule`).then(response => {
+			this.setState({ proxyRule: response.data });
+			this.setState({ proxyRuleLoading: false });
+		});
+	}
+	render() {
+		var { proxyRule, proxyRuleLoading } = this.state;
+		return <>
+			<form onSubmit={e => { this.connect(); e.preventDefault(); }}>
+				target: <input value={this.state.endpoint} onChange={e => { this.setState({ endpoint: e.target.value }); }}></input>
+				<button>connect</button>
+			</form>
+			<section>
+				<h4>proxy rules</h4>
+				<p>{proxyRuleLoading && "loading..."}</p>
+				{proxyRule && (
+					Object.entries(proxyRule).length ?
+						<table>{
+							Object.entries(proxyRule)
+								.map(([name, value]) => <tr key={name}>
+									<td>{name}</td>
+									<td>{value}</td>
+								</tr>)
+						}</table> :
+						"(no proxy rules)"
+				)}
+			</section>
+		</>;
+	}
+}
