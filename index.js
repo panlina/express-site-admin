258c41ec2c81eef540dc28d2b46581b7e02451f1
Jack Lu <jacklu@jacklu.me>
2020-04-17 10:42:26 +0800

    +add proxy rule

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ class Index extends React.Component {
 		this.state = {
 			endpoint: "",
 			proxyRule: undefined,
-			proxyRuleLoading: false
+			proxyRuleLoading: false,
+			newProxyRule: { name: '', value: '' },
+			proxyRuleAdding: false,
+			proxyRuleAdded: undefined
 		};
 	}
 	connect() {
@@ -25,8 +28,23 @@ class Index extends React.Component {
 			this.setState({ proxyRuleLoading: false });
 		});
 	}
+	addProxyRule() {
+		this.setState({ proxyRuleAdding: true });
+		var { name, value } = this.state.newProxyRule;
+		axios.put(`${this.state.endpoint}/proxy-rule/${encodeURIComponent(name)}`, JSON.stringify(value), { headers: { 'Content-Type': 'application/json' } }).then(response => {
+			this.setState({ proxyRuleAdded: response.data });
+			this.setState({ proxyRuleAdding: false });
+			this.state.proxyRule[name] = value;
+			this.state.newProxyRule.name = '';
+			this.state.newProxyRule.value = '';
+			this.setState(this.state);
+		}, error => {
+			this.setState({ proxyRuleAdded: error });
+			this.setState({ proxyRuleAdding: false });
+		});
+	}
 	render() {
-		var { proxyRule, proxyRuleLoading } = this.state;
+		var { proxyRule, proxyRuleLoading, newProxyRule, proxyRuleAdding, proxyRuleAdded } = this.state;
 		return <>
 			<form onSubmit={e => { this.connect(); e.preventDefault(); }}>
 				target: <input value={this.state.endpoint} onChange={e => { this.setState({ endpoint: e.target.value }); }}></input>
@@ -38,15 +56,29 @@ class Index extends React.Component {
 				{proxyRule && (
 					proxyRule instanceof Error ?
 						`error: ${proxyRule.response && proxyRule.response.data || proxyRule.message}` :
-						Object.entries(proxyRule).length ?
-							<table>{
-								Object.entries(proxyRule)
-									.map(([name, value]) => <tr key={name}>
-										<td>{name}</td>
-										<td>{value}</td>
-									</tr>)
-							}</table> :
-							"(no proxy rules)"
+						<>
+							<table>
+								{
+									Object.entries(proxyRule).length ?
+										Object.entries(proxyRule)
+											.map(([name, value]) => <tr key={name}>
+												<td>{name}</td>
+												<td>{value}</td>
+											</tr>) :
+										[<tr><td colSpan={2}>(no proxy rules)</td></tr>]
+								}
+								<tr>
+									<td><input form="add-proxy-rule" disabled={proxyRuleAdding} value={newProxyRule.name} onChange={e => { this.state.newProxyRule.name = e.target.value; this.setState(this.state); }} /></td>
+									<td><input form="add-proxy-rule" disabled={proxyRuleAdding} value={newProxyRule.value} onChange={e => { this.state.newProxyRule.value = e.target.value; this.setState(this.state); }} /></td>
+									<td>
+										{!proxyRuleAdding && <button form="add-proxy-rule" title="add">➕</button>}
+										{proxyRuleAdding && "(adding..)"}
+										{proxyRuleAdded instanceof Error && `error: ${proxyRuleAdded.response && proxyRuleAdded.response.data || proxyRuleAdded.message}`}
+									</td>
+								</tr>
+							</table>
+							<form id="add-proxy-rule" onSubmit={e => { this.addProxyRule(); e.preventDefault(); }}></form>
+						</>
 				)}
 			</section>
 		</>;
